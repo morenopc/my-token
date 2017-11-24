c4e2fb637440ca4636268a6ff03a09fffab96e65
Moreno Cunha <moreno.pinheiro@gmail.com>
2017-11-24 00:00:19 -0200

    Add setPrices, buy and sell functions can make the token's value be backed by ether (or other tokens) by creating a fund that automatically sells and buys them at market value

--- my-token.sol
+++ my-token.sol
@@ -23,6 +23,9 @@ contract MyToken is owned {
     string public symbol;
     uint8 public decimals;
 
+    uint256 public sellPrice;
+    uint256 public buyPrice;
+
     /* This creates an array with all balances */
     mapping (address => uint256) public balanceOf;
     mapping (address => bool) public frozenAccount;
@@ -44,6 +47,30 @@ contract MyToken is owned {
         name = tokenName;                                   // Set the name for display purposes
         symbol = tokenSymbol;                               // Set the symbol for display purposes
         decimals = decimalUnits;                            // Amount of decimals for display purposes
+    }
+
+    function setPrices(uint256 newSellPrice, uint256 newBuyPrice) onlyOwner public {
+        sellPrice = newSellPrice;
+        buyPrice = newBuyPrice;
+    }
+
+    function buy() payable public returns (uint amount) {
+        amount = msg.value / buyPrice;                    // calculates the amount
+        require(balanceOf[this] >= amount);               // checks if it has enough to sell
+        balanceOf[msg.sender] += amount;                  // adds the amount to buyer's balance
+        balanceOf[this] -= amount;                        // subtracts amount from seller's balance
+        Transfer(this, msg.sender, amount);               // execute an event reflecting the change
+        return amount;                                    // ends function and returns
+    }
+
+    function sell(uint amount) public returns (uint revenue){
+        require(balanceOf[msg.sender] >= amount);         // checks if the sender has enough to sell
+        balanceOf[this] += amount;                        // adds the amount to owner's balance
+        balanceOf[msg.sender] -= amount;                  // subtracts the amount from seller's balance
+        revenue = amount * sellPrice;
+        require(msg.sender.send(revenue));                // sends ether to the seller: it's important to do this last to prevent recursion attacks
+        Transfer(msg.sender, this, amount);               // executes an event reflecting on the change
+        return revenue;                                   // ends function and returns
     }
 
     function mintToken(address target, uint256 mintedAmount) onlyOwner public {
