477174a56d10b86b5e8e76a4539aacb244d0c0c8
Moreno Cunha <moreno.pinheiro@gmail.com>
2017-11-23 22:44:04 -0200

    Add constructor function

--- my-token.sol
+++ my-token.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.4.18;
 contract MyToken {
     /* This creates an array with all balances */
     mapping (address => uint256) public balanceOf;
+
+    function MyToken(uint256 initialSupply) public {
+        balanceOf[msg.sender] = initialSupply;
+    }
 }
