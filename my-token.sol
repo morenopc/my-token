461be35e56c9711b3c2efcb45a5d95e0dc035c02
Moreno Cunha <moreno.pinheiro@gmail.com>
2017-11-23 22:53:06 -0200

    Add an event notification function

--- my-token.sol
+++ my-token.sol
@@ -8,6 +8,8 @@ contract MyToken {
     /* This creates an array with all balances */
     mapping (address => uint256) public balanceOf;
 
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
     /* Initializes contract with initial supply tokens to the creator of the contract */
     function MyToken(
         uint256 initialSupply,
@@ -29,5 +31,8 @@ contract MyToken {
         /* Add and subtract new balances */
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
+
+        /* Notify anyone listening that this transfer took place */
+        Transfer(msg.sender, _to, _value);
     }
 }
