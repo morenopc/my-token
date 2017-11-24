63d0947f531f6ae71e8035c3d7c3b30dfe9df131
Moreno Cunha <moreno.pinheiro@gmail.com>
2017-11-23 23:12:23 -0200

    Add an internal transfer function which can only be called by the contract itself

--- my-token.sol
+++ my-token.sol
@@ -23,6 +23,18 @@ contract MyToken {
         decimals = decimalUnits;                            // Amount of decimals for display purposes
     }
 
+    /* Internal transfer, only can be called by this contract */
+    function _transfer(address _from, address _to, uint _value) internal {
+        require (_to != 0x0);                               // Prevent transfer to 0x0 address. Use burn() instead
+        require (balanceOf[_from] > _value);                // Check if the sender has enough
+        require (balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
+        require(!frozenAccount[_from]);                     // Check if sender is frozen
+        require(!frozenAccount[_to]);                       // Check if recipient is frozen
+        balanceOf[_from] -= _value;                         // Subtract from the sender
+        balanceOf[_to] += _value;                           // Add the same to the recipient
+        Transfer(_from, _to, _value);
+    }
+
     /* Send coins */
     function transfer(address _to, uint256 _value) public {
         /* Check if sender has balance and for overflows */
